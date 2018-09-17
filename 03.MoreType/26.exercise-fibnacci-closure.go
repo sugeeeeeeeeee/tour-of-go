e8f20c1c4f5d68c5b6326e1bad9e7f1fdff5a865
sugeeeeeeeeee <sugeeeeeeeeee@gmail.com>
2018-09-17 10:29:44 +0000

    commit all

--- 03.MoreType/26.exercise-fibnacci-closure.go
+++ 03.MoreType/26.exercise-fibnacci-closure.go
@@ -5,11 +5,18 @@ import "fmt"
 // fibonacci is a function that returns
 // a function that returns an int.
 func fibonacci() func() int {
+	a, b := 0, 1
+	fmt.Printf("%d\n%d\n", a, b)
+	return func() int {
+		a, b = b, a+b
+		fmt.Println(b)
+		return b
+	}
 }
 
 func main() {
 	f := fibonacci()
 	for i := 0; i < 10; i++ {
-		fmt.Println(f())
+		f()
 	}
 }
