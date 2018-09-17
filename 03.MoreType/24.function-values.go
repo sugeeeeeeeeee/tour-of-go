341616785f30e64e1540e2eac0b743f95cece7f6
sugeeeeeeeeee <sugeeeeeeeeee@gmail.com>
2018-09-17 10:17:23 +0000

    commit all

--- 03.MoreType/24.function-values.go
+++ 03.MoreType/24.function-values.go
@@ -1,3 +1,16 @@
+/*
+hypot := func(x, y float64) float64 { return math.Sqrt(x*x + y*y) }
+上記のように関数も変数として扱える。
+※無名関数(lambda)みたい。
+
+浮動小数点xとyを引数として渡して
+x*x+y*yの平方根を求める関数を返す関数をhypot変数として定義する
+
+fmt.Println(hypot(5,12))だと√(5*5+12*12)が帰ってくる
+fmt.Println(compute(hypot))だと、上記で定義したhypotを引数として
+compute関数にfnで渡して、fn(3,4)をreturnとする。
+
+*/
 package main
 
 import (
