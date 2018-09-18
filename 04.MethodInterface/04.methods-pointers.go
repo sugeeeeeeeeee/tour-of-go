0f056c0d8c914593d6cfc11ba39ee0e711601e25
sugeeeeeeeeee <sugeeeeeeeeee@gmail.com>
2018-09-18 10:38:06 +0000

    commit all

--- 04.MethodInterface/04.methods-pointers.go
+++ 04.MethodInterface/04.methods-pointers.go
@@ -1,3 +1,6 @@
+/*
+ポインタレシーバを持つメソッドは、レシーバが指す変数を変更できる。
+*/
 package main
 
 import (
@@ -13,9 +16,9 @@ func (v Vertex) Abs() float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
-func (v *Vertex) Scale(f float64) {
-	v.X = v.X * f
-	v.Y = v.Y * f
+func (v *Vertex) Scale(f float64) { // <- ポインタレシーバを持つメソッド
+	v.X = v.X * f // <- vの値を変更できる。この例だと*10している
+	v.Y = v.Y * f // <- 同上
 }
 
 func main() {
