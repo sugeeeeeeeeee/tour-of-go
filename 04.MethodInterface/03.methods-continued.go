845148c2d4a6b4a20b7e7d383f1d4aaf0efb4d46
sugeeeeeeeeee <sugeeeeeeeeee@gmail.com>
2018-09-18 08:16:30 +0000

    commit all

--- 04.MethodInterface/03.methods-continued.go
+++ 04.MethodInterface/03.methods-continued.go
@@ -1,3 +1,6 @@
+/*
+struct型だけじゃなく、typeに対してもメソッドを定義することが可能。
+*/
 package main
 
 import (
@@ -7,7 +10,7 @@ import (
 
 type MyFloat float64
 
-func (f MyFloat) Abs() float64 {
+func (f MyFloat) Abs() float64 { // MyFloatが値レシーバ、-π(-3.14159265.....)を取得する。
 	if f < 0 {
 		return float64(-f)
 	}
@@ -17,4 +20,10 @@ func (f MyFloat) Abs() float64 {
 func main() {
 	f := MyFloat(-math.Sqrt2)
 	fmt.Println(f.Abs())
+
+	f1 := MyFloat(-1.11111111)
+	fmt.Println(f1.Abs())
+
+	f2 := MyFloat(2.22222222)
+	fmt.Println(f2.Abs())
 }
